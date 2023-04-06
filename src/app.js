0182c967b8b03ec36e4a5f3dbcd5dd0c03fa3482
Lorena <blorena1321@gmail.com>
2023-04-06 16:34:13 -0300

    inicio projeto

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/teste", (req, res) => {
+    res.send("deu certo carai");
+})
+
+app.post("/testando", (req, res)=>{
+    res.send("postouuuuu");
+})
+
+
+
+app.listen(5000, ()=> console.log("servidor rodando no 5000"));
