709a04ac31f8263339778c73874ab3f7069ef42d
Lorena <blorena1321@gmail.com>
2023-04-09 12:28:23 -0300

    últimos requisitos

--- src/app.js
+++ src/app.js
@@ -7,23 +7,31 @@ app.use(express.json());
 
 const usuario = [];
 const tweets = [];
+let avatar = '';
 
 app.post("/sign-up", (req, res) => {
-    const { username, avatar } = req.body;
-    const userON = { username, avatar};
+    const { username, avatar: userAvatar } = req.body;
+    avatar = userAvatar;
+    const userON = { username, avatar: userAvatar};
     usuario.push(userON);
     res.send("OK");
     console.log(usuario);
 })
 
 app.get("/tweets", (req, res) => {
-const lastTweet = tweets.slice(-10);
+const lastTweet = tweets.slice(-10).map(t => ({ ...t, avatar}));
     res.send(lastTweet);
 })
 
 app.post("/tweets", (req, res) => {
     const { username, tweet } = req.body;
-    const tweetON = { username, tweet };
+
+    const user = usuario.find(u => u.username === username);
+    if(!user){
+        res.status(401).send("UNAUTHORIZED");
+        return;
+    }
+    const tweetON = { username, tweet};
     tweets.push(tweetON);
     res.send(tweetON);
 })
